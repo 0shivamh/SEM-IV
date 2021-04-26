3696305c340b447aba1c5110f3b6e9d671cf658b
aishivam <shivamhande0000@gmail.com>
2021-04-26 11:29:33 +0530

    Write a program to implement an address book with options given below:     a) Create address book. b) View address book. c) Insert a record. d) Delete a record. e)     Modify a record. f) Exit

--- EOS/Ex1.cpp
+++ EOS/Ex1.cpp
@@ -1,7 +1,7 @@
 /*
-Write a program to implement an address book with options given below:
-a) Create address book. b) View address book. c) Insert a record. d) Delete a record. e)
-Modify a record. f) Exit
+    Write a program to implement an address book with options given below:
+    a) Create address book. b) View address book. c) Insert a record. d) Delete a record. e)
+    Modify a record. f) Exit
 */
 #include<iostream>
 #include<stdlib.h>
